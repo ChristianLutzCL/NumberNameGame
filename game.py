f900a390eef3250ce07bf71c3fb5c63b7cb3e85c
LosBopfos <christian.lutz.home@gmx.de>
2017-08-08 17:31:01 +0200

    Code update. Added some turtle functions

--- game.py
+++ game.py
@@ -1,22 +1,35 @@
 import random
 import turtle
 
+my_turtle = turtle.Turtle()
+
+def turtle():
+    for i in range(4):
+        my_turtle.forward(100)
+        my_turtle.right(90)
+
 def number_generator(number_one, number_two):
     #Generate a random number and return it
     generated_number = random.randrange(number_one, number_two)
     return generated_number
 
-
 def check_number():
-    if input_nr == gen_nr:
-        print("YES!")
-    elif input_nr > gen_nr:
-        print("Your numeber is too big!")
-    elif input_nr < gen_nr:
-        print("Your number is too small!")
-    else:
-        print("NOPE!")
+    while True:
+        # save input into a variable
+        input_nr = int(input("Your Number: "))
+        my_turtle.forward(input_nr)
 
+        # Try to find the correct number. If found, break the while loop
+        if input_nr == gen_nr:
+            print("Yes!")
+            turtle()
+            break
+        elif input_nr > gen_nr:
+            print("Too big")
+        elif input_nr < gen_nr:
+            print("Too small")
+        else:
+            print("Wrong!")
 
 #Input the range to guess
 print("Set a range!")
@@ -26,8 +39,6 @@ input_two = int(input("Your number two: "))
 # Start function and store generated number into a variable
 gen_nr = number_generator(input_one, input_two)
 
-print(gen_nr)
+print(gen_nr) # gen_nr to read
 
-# save input into a variable
-input_nr = int(input("Your Number: "))
 check_number()

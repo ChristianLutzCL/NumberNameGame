17a54356bde2e22a3cdd0a17d1de6953c6967f71
LosBopfos <christian.lutz.home@gmx.de>
2017-08-12 00:38:51 +0200

    Changed the most of the code

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This is a simple number guessing game combined with Python's Turtle function.
 
-![Picture of the Game with Python code](https://i.gyazo.com/ff061a021338b4cc8ab93de3895fa22d.png)
+![Picture of the Game with Python code](https://i.gyazo.com/ff061a021338b4cc8ab93de3895fa227.png)
--- game.py
+++ game.py
@@ -1,118 +1,103 @@
+#NumberGame and NameGame
+#LosBopfos 2017
+
+#---- IMPORT ----
 import random
-import turtle
-import urllib
-import os
 
 
-my_turtle = turtle.Turtle()
 
-#def test():
-
-def turtle(n):
-    for i in range(n):
-        my_turtle.forward(100)
-        my_turtle.right(n + 10)
-
+#---- FUNCTIONS ----
 def number_generator(number_one, number_two):
     #Generate a random number and return it
     generated_number = random.randrange(number_one, number_two)
     return generated_number
 
 
-def dec_n():
-    print("Test")
-    input_one = int(input("Nummer eins: "))
-    input_two = int(input("Nummer zwei: "))
+def home():
+    print("----MENU----")
+    print("[A] - NumberGame")
+    print("[B] - NameGame")
+    print("[Q] - Quit")
+    print("------------")
 
-    random_nr = number_generator(input_one, input_two)
+    decision = input("Your decision: ")
 
-    input_saved = []
+    if decision == "A" or decision == "a":
+        number_game()
+    elif decision == "B" or decision == "b":
+        name_game()
+    elif decision == "Q" or decision == "q":
+        quit()
+    else:
+        print("Wrong decision. Please try again!")
+        home()
+
+
+def number_game():
+    points = 0
+
+    print("Welcome to NumberGame!")
+    print("Set a range to guess")
+    num1 = int(input("Number one: "))
+    num2 = int(input("Number two: "))
+
+    random_number = number_generator(num1, num2)
 
     while True:
+        your_number = int(input("Your number: "))
 
-        input_random = int(input("Deine Nummer: "))
-
-        if random_nr == input_random:
-            print("Genau!")
-            input_saved.append(input_random)
-            print(input_saved)
-
-            turtle(input_saved[0])
-
-
+        if your_number == random_number:
+            print("Correct!")
+            points += 1
+            print(points)
             home()
             break
-        elif random_nr > input_random:
-            print("Nummer zu klein")
-            input_saved.append(input_random)
-            print(input_saved)
-        elif random_nr < input_random:
-            print("Nummer zu groß")
-            input_saved.append(input_random)
-            print(input_saved)
+        elif your_number > random_number:
+            print("Too high!")
+        elif your_number < random_number:
+            print("Too small!")
         else:
-            print("Fehler!")
+            print("Something gone wrong...")
 
 
+def name_game():
+    print("Welcome to NameGame!")
+    print("You have to advise the right names based on some hints.")
+
+    names = ["Peter", "Franz", "Holger", "Anna", "Nina", "Lena", "Lea", "Lara"]
+
+    name_len = len(names)
+    choosen_name = names[random.randrange(0, name_len)]
+    print(choosen_name)
+
+    name_checker(choosen_name)
 
 
-def dec_a():
-    print("Test")
+def name_checker(c_name):
 
-def dec_s():
-    print("Test")
-
-def home():
-    print("------Welcome------")
-    print("Choose the type of the game")
-    print("-N- Normal")
-    print("-A- Advanced")
-    print("-S- Simple")
-
-
-    decision = input("Decision: ")
-
-    if decision == "N" or decision == "n":
-        print("Pressed: N")
-        dec_n()
-    elif decision == "A" or decision == "a":
-        print("Pressed: A")
-        dec_a()
-    elif decision == "S" or decision == "s":
-        print("Pressed: S")
-        dec_s()
-    else:
-        print("Wrong decision")
-
-
-def check_number():
     while True:
-        # save input into a variable
-        input_nr = int(input("Your Number: "))
-        my_turtle.forward(input_nr)
+        name = c_name
+        name_len = len(name)
 
-        # Try to find the correct number. If found, break the while loop
-        if input_nr == gen_nr:
-            print("Yes!")
-            turtle()
+        random_pos = random.randrange(0, name_len)
+        letter = name[random_pos]
+
+        print("The length of the name is " + str(name_len))
+        print("The letter on position " + str(random_pos) + " is " + str(letter))
+
+        input_name = input("Name: ")
+
+        if input_name == name:
+            print("Wow! You Won!")
+            home()
             break
-        elif input_nr > gen_nr:
-            print("Too big")
-        elif input_nr < gen_nr:
-            print("Too small")
         else:
-            print("Wrong!")
+            print("Sorry, wrong...")
+
+
+def quit():
+    print("quit")
+
+
 
 home()
-#Input the range to guess
-#print("Set a range!")
-#input_one = int(input("Your number one: "))
-#input_two = int(input("Your number two: "))
-
-# Start function and store generated number into a variable
-#gen_nr = number_generator(input_one, input_two)
-
-#print(gen_nr) # gen_nr output
-
-
-#check_number()
